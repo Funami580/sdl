696d2e8ed4377e5e8d1c25250b4284f9a618b1ce
Funami580 <63090225+Funami580@users.noreply.github.com>
2024-02-29 14:56:43 +0100

    fix voe extractor

--- src/extractors/voe.rs
+++ src/extractors/voe.rs
@@ -1,4 +1,5 @@
 use anyhow::Context;
+use base64::Engine;
 use once_cell::sync::Lazy;
 use regex::Regex;
 
@@ -19,14 +20,34 @@ impl Extractor for Voe {
     }
 
     async fn extract_video_url(from: ExtractFrom) -> Result<ExtractedVideo, anyhow::Error> {
-        static VIDEO_URL_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r#"'hls': '([^']+)'"#).unwrap());
+        static VIDEO_URL_REGEX: Lazy<Regex> = Lazy::new(|| {
+            Regex::new(
+                r#"let \w+ = '((?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}={2}))';"#,
+            )
+            .unwrap()
+        });
 
         let source = from.get_source(None).await?;
         VIDEO_URL_REGEX
             .captures(&source)
             .and_then(|captures| captures.get(1))
+            .and_then(|capture| {
+                base64::prelude::BASE64_STANDARD
+                    .decode(capture.as_str())
+                    .ok()
+                    .map(|mut reversed_json| {
+                        reversed_json.reverse();
+                        reversed_json
+                    })
+            })
+            .and_then(|json_text| serde_json::from_slice::<serde_json::value::Value>(&json_text).ok())
+            .and_then(|json| {
+                json.as_object()
+                    .and_then(|json_object| json_object.get("file"))
+                    .and_then(|json_file| json_file.as_str().map(|s| s.to_string()))
+            })
             .map(|video_url| ExtractedVideo {
-                url: video_url.as_str().to_string(),
+                url: video_url,
                 referer: None,
             })
             .with_context(|| "Voe: failed to retrieve sources")
@@ -40,11 +61,10 @@ mod tests {
 
     #[tokio::test]
     async fn test_voe() {
-        let source = "var sources = {↵
-            'hls': 'https://delivery-node-p529oubjmokvzzdm.voe-network.net/engine/hls2-c/01/09512/ytd65pzpecoo_,n,.urlset/master.m3u8?t=1jRM2PpnYUY4QwQMhav3le-633OJQgg8HaOovs0Tf18&s=1697938932&e=14400&f=47560621&node=delivery-node-ynug3prrg0f4gget.voe-network.net&i=185.249&sp=2500&asn=12329',↵
-            'video_height': 720,↵
-                    };↵";
-        let expected = "https://delivery-node-p529oubjmokvzzdm.voe-network.net/engine/hls2-c/01/09512/ytd65pzpecoo_,n,.urlset/master.m3u8?t=1jRM2PpnYUY4QwQMhav3le-633OJQgg8HaOovs0Tf18&s=1697938932&e=14400&f=47560621&node=delivery-node-ynug3prrg0f4gget.voe-network.net&i=185.249&sp=2500&asn=12329";
+        let source = "<script>
+        let f62aad852c654bf8c9737da67c45630c7dec5019 = 'fTA6Imtjb2xiZGFfbm9pdGF6aXRlbm9tIiwwOiJlbGlmb3JwIiwwOiJtdWltZXJwIiwwOiJwZm8iLCIudGkgZGFvbCBvdCBnbml5cnQgZWxpaHcgcm9ycmUgbmEgc2F3IGVyZWh0IHJvICxkZWRhb2wgZWIgdCduZGx1b2MgKW9lZGl2IGduaW1hZXJ0cyByb2YgZGVzdSBlbGlmIGEoIHRzZWZpbmFtIFNMSCBlaFQgLmtyb3d0ZW4gZWh0IGh0aXcgbWVsYm9ycCBhIHMnZXJlaHQgdHViICx5cnJvUyI6Im5vaXRhbmFscHhlX3JvcnJlX2tyb3d0ZW4iLCJuaWFnYSByZXRhbCB5clQgLSByb3JyRSBrcm93dGVOIjoicm9ycmVfa3Jvd3RlbiIsZXNsYWY6ImRhb2xfb3R1YSIsMDAwMDAwMDU6ImV6aXNfcmVmZnViIiwwODE6Imh0Z25lbF9yZWZmdWIiLCIiOiJrY2FibGxhZiIsIjAyMzM9bnNhJjAwNTI9cHMmMjMyLjcxMj1pJnRlbi5rcm93dGVuLWVvdi4waTZtcWYxYmx1eHB4NzVxLWVkb24teXJldmlsZWQ9ZWRvbiYwOTk5ODg3PWYmMDA0NDE9ZSYyMzkyMTI5MDcxPXMma2NoeVJmRGh0b1BjQU9XV1BXRW84MUhYcXNMZkpHU1djSkpDc0tRdVRGRj10Pzh1M20ucmV0c2FtL1x0ZXNscnUuLG4sX3ljd20wdmJjamhzcC9cNzc1MTAvXDEwL1wyc2xoL1xlbmlnbmUvXHRlbi5rcm93dGVuLWVvdi4waTZtcWYxYmx1eHB4NzVxLWVkb24teXJldmlsZWQvXC9cOnNwdHRoIjoiZWxpZiIsMToiZGVibWUiLCJ5Y3dtMHZiY2poc3AiOiJlZG9jIiwxMzQ6ImVkb24iLCIua2NlaGMgbGF1bmFtIGEgcm9mIHRzaWwgYSBuaSBzbGlhdGVkIGVjaXZlZC9ccmVzd29yYiBydW95IGRlZGRhIGV2YWggZVcgLmRldHJvcHB1cyB0b24gc2kgZWNpdmVkL1xyZXN3b3JiIHJ1b3kgZWtpbCBza29vbCB0SSI6Im5vaXRhbmFscHhlX2RldHJvcHB1c190b25fcmVzd29yYiIsImRldHJvcHB1cyB0b24gZWNpdmVEL1xyZXN3b3JCIjoiZGV0cm9wcHVzX3Rvbl9yZXN3b3JiIns=';
+        </script>";
+        let expected = "https://delivery-node-q57xpxulb1fqm6i0.voe-network.net/engine/hls2/01/01577/pshjcbv0mwcy_,n,.urlset/master.m3u8?t=FFTuQKsCJJcWSGJfLsqXH18oEWPWWOAcPothDfRyhck&s=1709212932&e=14400&f=7889990&node=delivery-node-q57xpxulb1fqm6i0.voe-network.net&i=217.232&sp=2500&asn=3320";
 
         let extracted = Voe::extract_video_url(ExtractFrom::Source(source.to_string())).await;
         assert_eq!(extracted.unwrap().url, expected.to_string());
