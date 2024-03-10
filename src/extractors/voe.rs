b87f743e5600fb9aa4431ba8500a1adb82226ca5
Funami580 <63090225+Funami580@users.noreply.github.com>
2024-03-10 21:10:54 +0100

    fix voe again

--- src/extractors/voe.rs
+++ src/extractors/voe.rs
@@ -20,6 +20,30 @@ impl Extractor for Voe {
     }
 
     async fn extract_video_url(from: ExtractFrom) -> Result<ExtractedVideo, anyhow::Error> {
+        let source = from.get_source(None).await?;
+
+        match Self::extract1(&source).await {
+            Ok(video) => Ok(video),
+            Err(_) => Self::extract2(&source).await,
+        }
+    }
+}
+
+impl Voe {
+    async fn extract1(source: &str) -> Result<ExtractedVideo, anyhow::Error> {
+        static VIDEO_URL_REGEX: Lazy<Regex> = Lazy::new(|| Regex::new(r#"'hls': '([^']+)'"#).unwrap());
+
+        VIDEO_URL_REGEX
+            .captures(source)
+            .and_then(|captures| captures.get(1))
+            .map(|video_url| ExtractedVideo {
+                url: video_url.as_str().to_string(),
+                referer: None,
+            })
+            .with_context(|| "Voe: failed to retrieve sources")
+    }
+
+    async fn extract2(source: &str) -> Result<ExtractedVideo, anyhow::Error> {
         static VIDEO_URL_REGEX: Lazy<Regex> = Lazy::new(|| {
             Regex::new(
                 r#"let \w+ = '((?:[A-Za-z0-9+/]{4})*(?:[A-Za-z0-9+/]{4}|[A-Za-z0-9+/]{3}=|[A-Za-z0-9+/]{2}={2}))';"#,
@@ -27,9 +51,8 @@ impl Extractor for Voe {
             .unwrap()
         });
 
-        let source = from.get_source(None).await?;
         VIDEO_URL_REGEX
-            .captures(&source)
+            .captures(source)
             .and_then(|captures| captures.get(1))
             .and_then(|capture| {
                 base64::prelude::BASE64_STANDARD
@@ -60,7 +83,19 @@ mod tests {
     use crate::extractors::{ExtractFrom, Extractor};
 
     #[tokio::test]
-    async fn test_voe() {
+    async fn test_voe1() {
+        let source = "var sources = {
+            'hls': 'https://delivery-node-oxccnn9nkkcxh4ir.voe-network.net/engine/hls2-c/01/10111/n6odadstvbey_,n,.urlset/master.m3u8?t=X73hbD6gRBj2AD2BPe7eamyuaXyBrqJmCPXMLvapNsA&s=1710100837&e=14400&f=50556351&node=delivery-node-sq0tgp6zzeel6axe.voe-network.net&i=217.232&sp=2500&asn=3320',
+            'video_height': 720,
+                    };";
+        let expected = "https://delivery-node-oxccnn9nkkcxh4ir.voe-network.net/engine/hls2-c/01/10111/n6odadstvbey_,n,.urlset/master.m3u8?t=X73hbD6gRBj2AD2BPe7eamyuaXyBrqJmCPXMLvapNsA&s=1710100837&e=14400&f=50556351&node=delivery-node-sq0tgp6zzeel6axe.voe-network.net&i=217.232&sp=2500&asn=3320";
+
+        let extracted = Voe::extract_video_url(ExtractFrom::Source(source.to_string())).await;
+        assert_eq!(extracted.unwrap().url, expected.to_string());
+    }
+
+    #[tokio::test]
+    async fn test_voe2() {
         let source = "<script>
         let f62aad852c654bf8c9737da67c45630c7dec5019 = 'fTA6Imtjb2xiZGFfbm9pdGF6aXRlbm9tIiwwOiJlbGlmb3JwIiwwOiJtdWltZXJwIiwwOiJwZm8iLCIudGkgZGFvbCBvdCBnbml5cnQgZWxpaHcgcm9ycmUgbmEgc2F3IGVyZWh0IHJvICxkZWRhb2wgZWIgdCduZGx1b2MgKW9lZGl2IGduaW1hZXJ0cyByb2YgZGVzdSBlbGlmIGEoIHRzZWZpbmFtIFNMSCBlaFQgLmtyb3d0ZW4gZWh0IGh0aXcgbWVsYm9ycCBhIHMnZXJlaHQgdHViICx5cnJvUyI6Im5vaXRhbmFscHhlX3JvcnJlX2tyb3d0ZW4iLCJuaWFnYSByZXRhbCB5clQgLSByb3JyRSBrcm93dGVOIjoicm9ycmVfa3Jvd3RlbiIsZXNsYWY6ImRhb2xfb3R1YSIsMDAwMDAwMDU6ImV6aXNfcmVmZnViIiwwODE6Imh0Z25lbF9yZWZmdWIiLCIiOiJrY2FibGxhZiIsIjAyMzM9bnNhJjAwNTI9cHMmMjMyLjcxMj1pJnRlbi5rcm93dGVuLWVvdi4waTZtcWYxYmx1eHB4NzVxLWVkb24teXJldmlsZWQ9ZWRvbiYwOTk5ODg3PWYmMDA0NDE9ZSYyMzkyMTI5MDcxPXMma2NoeVJmRGh0b1BjQU9XV1BXRW84MUhYcXNMZkpHU1djSkpDc0tRdVRGRj10Pzh1M20ucmV0c2FtL1x0ZXNscnUuLG4sX3ljd20wdmJjamhzcC9cNzc1MTAvXDEwL1wyc2xoL1xlbmlnbmUvXHRlbi5rcm93dGVuLWVvdi4waTZtcWYxYmx1eHB4NzVxLWVkb24teXJldmlsZWQvXC9cOnNwdHRoIjoiZWxpZiIsMToiZGVibWUiLCJ5Y3dtMHZiY2poc3AiOiJlZG9jIiwxMzQ6ImVkb24iLCIua2NlaGMgbGF1bmFtIGEgcm9mIHRzaWwgYSBuaSBzbGlhdGVkIGVjaXZlZC9ccmVzd29yYiBydW95IGRlZGRhIGV2YWggZVcgLmRldHJvcHB1cyB0b24gc2kgZWNpdmVkL1xyZXN3b3JiIHJ1b3kgZWtpbCBza29vbCB0SSI6Im5vaXRhbmFscHhlX2RldHJvcHB1c190b25fcmVzd29yYiIsImRldHJvcHB1cyB0b24gZWNpdmVEL1xyZXN3b3JCIjoiZGV0cm9wcHVzX3Rvbl9yZXN3b3JiIns=';
         </script>";
